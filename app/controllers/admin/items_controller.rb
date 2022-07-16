b2b8cb68b226e89e10e7a8a2182fb60ca9a551fd
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-07-16 16:10:31 +0000

    [Add]new, show

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -9,11 +9,22 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find_by(params[:id])
+    @genre = @item.genre
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item.id)
   end
 
   def edit
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :tax_free_price, :sales_status)
   end
 end
