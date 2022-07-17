5d2b784eb1565414babde7bd45bd388736594ff6
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-07-17 04:05:01 +0000

    [Add] genre

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,14 +1,15 @@
 class Admin::GenresController < ApplicationController
-  def index
-    @genre = Genre.new
-    @genres = Genre.all
-  end
 
-  def create
-    @genre = Genre.new(genre_params)
-    @genre.save
-    redirect_to request.referrer
-  end
+
+
+
+
+
+
+
+
+
+
 
   def edit
     @genre = Genre.find_by(params[:id])
@@ -22,7 +23,5 @@ class Admin::GenresController < ApplicationController
 
   private
 
-  def genre_params
-    params.require(:genre).permit(:name)
-  end
+
 end
