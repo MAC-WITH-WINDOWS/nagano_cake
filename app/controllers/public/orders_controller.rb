d43dd7d58342b05807c50c9ceaa1d1a94d5917c8
EC2 Default User <ec2-user@ip-172-31-1-26.ap-northeast-1.compute.internal>
2022-07-17 13:40:18 +0000

    [add]注文情報確認画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,6 +3,11 @@ class Public::OrdersController < ApplicationController
   end
 
   def check
+    @order = Order.new(order_params)
+    @order.address_update
+    @cart_items = current_customer.carts_items
+    @cart_items.build
+    @total_price = 0
   end
 
   def finish
@@ -13,4 +18,11 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:peyment_method, :post_code, :address, :name)
+  end
+  
 end
