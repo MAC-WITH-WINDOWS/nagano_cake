a32c9ab42a961d77ebae94490f9970a11a0f2702
Alexander4649 <handyasu08@gmail.com>
2022-07-17 03:51:37 +0000

    [Add]注文履歴詳細製作中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,7 @@
 class Admin::OrdersController < ApplicationController
   def show
+    #@order = Order.find(params[:id])
+    @orders = Order.all
+    # @order.save ? (redirect_to request.referer) : (render :show) #link_toで使えない時に
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,9 @@
 class Order < ApplicationRecord
   belongs_to :customer
+  has_many :order_items,dependent: :destroy
+  
+  def sum_of_price
+    order_item.purchace_price * order_item.amount
+  end 
+  
 end
