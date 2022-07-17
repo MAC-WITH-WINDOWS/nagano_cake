eefe1bdd8180d0c30d5c2a66b51d2f4f4e914d4d
Alexander4649 <handyasu08@gmail.com>
2022-07-17 04:03:46 +0000

    [Add]安部さんとする合わせした箇所削除

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -5,26 +5,16 @@ class Admin::ItemsController < ApplicationController
     end
     
     def new
-      @item = Item.new
-      @genres = Genre.all
     end
     
     def show
     end
     
     def create
-      @item = Item.new(item_params)
-      @item.save
-      redirect_to admin_item_path(@item.id)
+
     end
     
     def edit
-    end
-    
-    private
-
-    def item_params
-      params.require(:item).permit(:image, :name, :introduction, :genre_id, :tax_free_price, :sales_status)
     end
 
 end
