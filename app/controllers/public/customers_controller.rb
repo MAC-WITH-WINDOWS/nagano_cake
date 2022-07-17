ff08919bc53f0da4c97aecadb3f7951ff22adbf4
EC2 Default User <ec2-user@ip-172-31-1-26.ap-northeast-1.compute.internal>
2022-07-17 03:58:23 +0000

    [add]退会機能

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -14,12 +14,19 @@ class Public::CustomersController < ApplicationController
   end
 
   def check
+    @customer = current_customer
   end
   
+  def out
+    @customer = current_customer
+    @customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
   
   private
   
     def configure_account_update_params
-        params.require(:customer).permit(:attribute, :last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number, :is_deleted)
+        params.require(:customer).permit(:email, :attribute, :last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number, :is_deleted)
     end
 end
