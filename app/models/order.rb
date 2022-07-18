df2609c94119e8cdffa5cd65edc0b0929c0d7b58
bbyya551 <106591130+bbyya551@users.noreply.github.com>
2022-07-18 04:04:16 +0000

    [Add] error edit

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,14 +1,14 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items,dependent: :destroy
-  
+
   def sum_of_price
     order_item.purchace_price * order_item.amount
   end
-  
-  
+
+
   enum order_status: {
      "入金待ち":0, "入金確認":1, "製作中":2, "発送準備中":3, "発送済":4
   }
-  
+
 end
