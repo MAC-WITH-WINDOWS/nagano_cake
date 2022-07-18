fdf6c52a9059f8a8aad795cb2ba0a98238826dc5
Alexander4649 <handyasu08@gmail.com>
2022-07-18 03:56:29 +0000

    [Add]注文履歴詳細製作中

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,6 +6,13 @@ class Order < ApplicationRecord
     order_item.purchace_price * order_item.amount
   end
   
+  def ship_address
+  '〒' + post_code + ' ' + address + ' ' + to_name
+  end
+  
+  def ordered_day
+    created_at.strftime("%Y-%m-%d")
+  end
   
   enum order_status: {
      "入金待ち":0, "入金確認":1, "製作中":2, "発送準備中":3, "発送済":4
