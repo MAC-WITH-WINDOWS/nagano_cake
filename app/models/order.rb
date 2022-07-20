e9bb067477622bf88662e1fab57202f9187a6be5
hiiro4 <subhouse.indog@gmail.com>
2022-07-20 04:45:07 +0000

    [Add]注文履歴

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,29 +2,9 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items,dependent: :destroy
 
-  # 配送先セレクトに合わせてorderテーブルに格納する配送先情報を更新
-  def address_update(select_address)
-    # 自分の住所の場合、自分の住所を格納する
-    if select_address == "0"
-      @order.post_code = current_customer.post_code
-      @order.address = current_customer.address
-      @order.to_name = current_customer.first_name + current_customer.last_name
-    # 登録済みの住所の場合、配送先を呼び出してその情報を格納する
-    elsif select_address == "1"
-      @address = Address.find(params[:order][:address_id])
-      @order.post_code = @address.post_code
-      @order.address = @address.address
-      @order.to_name = @address.to_name
-    end
-    # 新しい住所は入力した値がそのまま格納される
-  end
-
-
   def sum_of_price
     order_item.purchace_price * order_item.amount
   end
-
-
 
   def ship_address
     "〒 #{post_code} #{address} #{to_name}"
@@ -35,7 +15,7 @@ class Order < ApplicationRecord
   }
 
   enum payment_method: {
-    "クレジットカード":0, "銀行振込":1
+    "クレジットカード":"0", "銀行振込":"1"
   }
 
 
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,11 +1,11 @@
 class OrderItem < ApplicationRecord
   belongs_to :item
   belongs_to :order
-  
+
   enum maiking_status: {
      "着手不可":0, "製作待ち":1, "製作中":2, "製作完了":3
   }
-  
+
   def self.subtotal
     array = []
     all.each do |order_item|
@@ -13,5 +13,13 @@ class OrderItem < ApplicationRecord
     end
     array.sum
   end
-  
+
+  def tax_include_price
+    (purchace_price*1.1).round(0)
+  end
+
+  def s_total
+    tax_include_price*amount
+  end
+
 end
